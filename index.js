7e98f3d4c1ed3cefc0be77f557c0ab6d4155d69b
ZakarFin <zakarfin@gmail.com>
2016-12-11 21:42:10 +0200

    Use port from env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'pug');
 
 var lunch = require('./lunch');
@@ -41,6 +42,6 @@ app.get('/lunch.json', function (req, res) {
     });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
 });
