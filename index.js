ef82a23b46f4b6dd3087075acf0906364a34a619
Marko Kuosmanen <marko.kuosmanen@maanmittauslaitos.fi>
2017-05-24 09:11:02 +0300

    Added Sevilla restaurant

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var path = require('path');
 
-var express = require('express')
+var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
