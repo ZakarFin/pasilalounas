b991f87945df1195b5b66a5c8e717231e1f1d544
Sami Mäkinen <zakarfin@gmail.com>
2018-02-24 21:38:14 +0200

    Disconnect after getting menus/after minute to prevent unnecessary resource usage

--- public/index.js
+++ public/index.js
@@ -1,14 +1,14 @@
 var socket = io();
 var main = document.getElementById('main');
-
+var counter = 9999;
 socket.on('places', function(msg) {
+    counter = msg.length;
     console.log('places', msg);
 });
 socket.on('menu', function(msg) {
     var container = document.getElementById('place_' + msg.id);
     var list = container.querySelector('.panel-body ul');
     list.removeChild(list.childNodes[0]);
-    console.log('menu for ' + msg.name, msg);
     if(!msg.menu || !msg.menu[currentDay]) {
         list.appendChild(createListItem('Listaa ei saatu ladattua.', true));
         list.appendChild(createLinkItem(msg.url,'Katso ravintolan sivuilta >>'));
@@ -18,7 +18,21 @@ socket.on('menu', function(msg) {
             list.appendChild(createListItem(menuItem));
         });
     }
+    counter--;
+    if(counter === 0) {
+        // got all the menus -> disconnect
+        socket.close();
+        console.log('Close socket');
+    }
+    console.log('menu for ' + msg.name, msg);
 });
+
+setTimeout(function() {
+    if(counter !== 0) {
+        socket.close();
+        console.log('Closing socket after a minute of waiting for menus. ' + counter + ' menus didnt load.');
+    }
+}, 60000);
 
 function createListItem(value, insertBreaks) {
     var li = document.createElement("li");
